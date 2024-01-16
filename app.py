40584e79b0d85b049b3604d2ab1bec3c7885a023
stephanosiliadis <stefanos06@outlook.com.gr>
2024-01-16 20:07:19 +0200

    Added requirements and updated code

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from validate_email import validate_email
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -10,7 +12,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
     email = db.Column(db.String(64))
-    password = db.Column(db.String(64))
+    password = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return f"User(username={self.username}, email={self.email}, password={self.password})"
@@ -27,13 +35,12 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        used_password = User.query.filter_by(password=password).first()
-
-        if used_password is not None:
-            return redirect(f"/home/{username}")
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            return "Invalid username or password."
 
         else:
-            return "No user found with that credentials..."
+            return redirect(f"/home/{username}")
 
     else:
         return render_template("login.html")
@@ -45,10 +52,23 @@ def register():
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
+
+        # Check if the email is valid and properly formatted
+        is_valid = validate_email(email, check_mx=True)
+
+        if not is_valid:
+            return "The provided email is not valid. Please provide a valid email."
+
         used_password = User.query.filter_by(password=password).first()
+        used_username = User.query.filter_by(username=username).first()
+        used_email = User.query.filter_by(email=email).first()
+
+        if used_username is not None or used_email is not None:
+            return "This username or email is already taken. Please choose a different one."
 
         if used_password is None:
-            user = User(username=username, email=email, password=password)
+            user = User(username=username, email=email)
+            user.set_password(password)  # Hash password before storing
             try:
                 db.session.add(user)
                 db.session.commit()
